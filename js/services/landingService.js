d6a94d0b43dd80b63586285adb2fc2a525372896
Milton SHerrera <mlton.s.herrera@live.com>
2026-04-28 09:23:07 -0500

    actualizar la función getLandingData para usar BASE_PATH y manejar errores de respuesta

--- js/services/landingService.js
+++ js/services/landingService.js
@@ -1,7 +1,15 @@
+import { BASE_PATH } from "../config.js";
+
 export async function getLandingData() {
     try {
-        const res = await fetch("../data/landing.json");
-        return await res.json();
+        const response = await fetch(`${BASE_PATH}/data/landing.json`);
+
+        if (!response.ok) {
+            throw new Error("Error al cargar landing");
+        }
+
+        return await response.json();
+
     } catch (error) {
         console.error("Error cargando landing:", error);
         return null;
